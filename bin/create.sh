3b7ce881607e81ef50a07de8979ac9b25e6229dc
Matteo Cusinato <akymos@users.noreply.github.com>
2023-05-06 11:56:12 +0200

    create post tool

---
+++ bin/create.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+read -p "Enter the post title: " POST_TITLE
+read -p "Enter the post category: " POST_CATEGORY
+
+TITLE_SLUG="$(printf -- "$POST_TITLE" | sed -E 's/[^a-zA-Z0-9]+/-/g' | tr "[:upper:]" "[:lower:]")"
+CATEGORY_SLUG="$(printf -- "$POST_CATEGORY" | sed -E 's/[^a-zA-Z0-9]+/-/g' | tr "[:upper:]" "[:lower:]")"
+
+POST_DATE="$(date +%Y-%m-%d)"
+
+POST_LAYOUT=$(cat <<EOF
+---
+layout: post
+title: $POST_TITLE
+date: $POST_DATE 00:00
+category: $CATEGORY_SLUG
+author: Akymos
+tags: [$CATEGORY_SLUG]
+anchor: true
+---
+
+<hr>
+<h2>Table of Contents</h2>
+<nav class="toc">
+* toc
+{:toc}
+</nav>
+<hr>
+<div class="pb-1" />
+
+
+EOF
+)
+
+POST_FILE="_posts/$CATEGORY_SLUG/$POST_DATE-$TITLE_SLUG.md"
+
+mkdir -p "_posts/$CATEGORY_SLUG"
+touch $POST_FILE
+echo "$POST_LAYOUT" > $POST_FILE
+
+exit 0
